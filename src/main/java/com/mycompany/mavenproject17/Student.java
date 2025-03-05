796e0c773870e709c377f15be0db939f8da23ab6
aishanoor1439 <aisha.noor1439@gmail.com>
2025-03-05 16:55:55 +0100

    Third Commit
    updated code

--- src/main/java/com/mycompany/mavenproject17/Student.java
+++ src/main/java/com/mycompany/mavenproject17/Student.java
@@ -2,18 +2,18 @@ package com.mycompany.mavenproject17;
 
 //Class Definition
 public class Student {
+    
 //    Attributes
-
-    String name = "Sana";
+    String name;
     private static String university = "Bahria";
     private static int semester = 2;
-    private static int totStudents = 0;
+    private static int rollNo;
 
 //  Methods
 //  Constructors
     Student(String name) {
         this.name = name;
-        totStudents++;
+        rollNo++;
     }
     
     void display(){
@@ -23,7 +23,7 @@ public class Student {
     static void displayStatic(){
         System.out.println("Semester:" + semester);
         System.out.println("University:" + university);
-        System.out.println("Total Students:" + totStudents);
+        System.out.println("Roll no:" + rollNo);
     }
     
     static void changeSemester(){
